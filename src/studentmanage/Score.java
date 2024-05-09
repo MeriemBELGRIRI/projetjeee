24bba67f89ed93e1cb3e73eb50440345c555c325
Lenovo T460s <Lenovo T460s@DESKTOP-MKMRTEA>
2024-05-09 14:43:32 +0100

    UPADATES2

--- src/studentmanage/Score.java
+++ src/studentmanage/Score.java
@@ -6,17 +6,20 @@ package studentmanage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Lenovo T460s
  */
 public class Score {
-    public void insertUpdateDeleteStudent(char operation, Integer Sid,Integer Cid,String descp) {
+    public void insertUpdateDeleteStudent(char operation, Integer Sid,Integer Cid,Double score,String descp) {
         Connection con = Connectionbd.getConnection();
         PreparedStatement ps;
     
@@ -29,7 +32,8 @@ public class Score {
                 // Assurez-vous d'attribuer les valeurs aux paramètres de la déclaration
                 ps.setInt(1, Sid);
                 ps.setInt(2, Cid);
-                ps.setString(3, descp);
+                ps.setDouble(3, score);
+                ps.setString(4, descp);
                 
               
                 // Exécutez la déclaration
@@ -40,47 +44,69 @@ public class Score {
                 Logger.getLogger(Course.class.getName()).log(Level.SEVERE, null, ex);
             } 
         }
-//         if (operation == 'u') {// u for update
-//        
-//            try {
-//                // Utilisez con.prepareStatement pour préparer la déclaration
-//                ps = con.prepareStatement("UPDATE `course` SET `label`= ?,`hours_number`= ? WHERE `id`=?");
-//                // Assurez-vous d'attribuer les valeurs aux paramètres de la déclaration
-//                ps.setString(1, Label);
-//                ps.setInt(2, hours);
-//                ps.setInt(3, id);
-//         
-//              
-//                // Exécutez la déclaration
-//                if (ps.executeUpdate() > 0) {
-//                    JOptionPane.showMessageDialog(null, " Le cours a ete modifiee");
-//                    
-//                }
-//            } catch (SQLException ex) {
-//                Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            
-//        }
-//         
-//         
-//          if (operation == 'd') {// u for delete
-//        
-//            try {
-//                // Utilisez con.prepareStatement pour préparer la déclaration
-//                ps = con.prepareStatement("DELETE FROM `score` WHERE `id`=?");
-//                // Assurez-vous d'attribuer les valeurs aux paramètres de la déclaration
-//              
-//                ps.setInt(1, id);
-//              
-//                // Exécutez la déclaration
-//                if (ps.executeUpdate() > 0) {
-//                    JOptionPane.showMessageDialog(null, " La note a ete Supprime");
-//                    
-//                }
-//            } catch (SQLException ex) {
-//                Logger.getLogger(student.class.getName()).log(Level.SEVERE, null, ex);
-//            } 
-//                
-//            }
+         if (operation == 'u') {// u for update
+        
+            try {
+                // Utilisez con.prepareStatement pour préparer la déclaration
+                ps = con.prepareStatement("UPDATE `score` SET `student_score`= ?,`description`= ? WHERE `course_id`=? AND `student_id`=?");
+                // Assurez-vous d'attribuer les valeurs aux paramètres de la déclaration
+                ps.setDouble(1, score);
+                ps.setString(2, descp);
+                ps.setInt(3, Cid);
+                ps.setInt(4, Sid);
+         
+              
+                // Exécutez la déclaration
+                if (ps.executeUpdate() > 0) {
+                    JOptionPane.showMessageDialog(null, " Le cours a ete modifiee");
+                    
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+         
+         
+          if (operation == 'd') {// d for delete
+        int YesOrNo = JOptionPane.showConfirmDialog(null,"Les notes liees au cours seront supprimer egalement","Supprimer",JOptionPane.OK_CANCEL_OPTION);
+        if(YesOrNo ==0){
+            try {
+                // Utilisez con.prepareStatement pour préparer la déclaration
+                ps = con.prepareStatement("DELETE FROM `score` WHERE `student_id`=? AND `course_id`=?");
+                // Assurez-vous d'attribuer les valeurs aux paramètres de la déclaration
+                ps.setInt(1, Sid);
+                ps.setInt(2, Cid);
+                // Exécutez la déclaration
+                if (ps.executeUpdate() > 0) {
+                    JOptionPane.showMessageDialog(null, " La note a ete Supprime");
+                    
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Score.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+                
+            }
+          }
       }  
+    public void fillScoreJtable(JTable table){
+        Connection con = Connectionbd.getConnection();
+        PreparedStatement ps;
+        try{
+            ps=con.prepareStatement("SELECT * FROM `score`");
+            ResultSet rs =ps.executeQuery();
+            DefaultTableModel model=(DefaultTableModel)table.getModel();
+            Object[] row;
+            while(rs.next()){
+            row=new Object[4];
+            row[0]=rs.getInt(1);
+            row[1]=rs.getInt(2);
+            row[2]=rs.getDouble(3);
+            row[3]=rs.getString(4);
+            model.addRow(row);
+            }
+        } catch (SQLException ex) {
+        Logger.getLogger(Course.class.getName()).log(Level.SEVERE, null, ex);
+    }
+   }
 }
